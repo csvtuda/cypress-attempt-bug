24a35dc9eaeb99802ec239cef8943c1cf803184d
Sebastian Vollbrecht <sebivo@freenet.de>
2024-12-20 17:11:50 +0100

    Add buggy setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
+const { writeFileSync } = require("node:fs");
+
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on("after:run", (results) => {
+        writeFileSync("results.json", JSON.stringify(results, null, 2));
+      });
+    },
+    retries: 2
   },
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://example.cypress.io');
+    // simulate a flaky test
+    if (Cypress.currentRetry < 2) {
+      expect(true).to.eq(false)
+    } else {
+      expect(true).to.eq(true)
+    }
   })
 })
